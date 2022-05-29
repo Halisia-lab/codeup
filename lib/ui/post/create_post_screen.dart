2ce5bf91de4e4759b205477eecd493d2d8bc216a
Halisia Halifa <halifa.halisia@gmail.com>
2022-05-29 22:04:46 +0200

    modifs

--- lib/ui/post/create_post_screen.dart
+++ lib/ui/post/create_post_screen.dart
@@ -2,9 +2,11 @@ import 'package:codeup/services/auth_service.dart';
 import 'package:codeup/services/post_service.dart';
 import 'package:codeup/ui/comment/comment_list_screen.dart';
 import 'package:codeup/ui/common/custom_app_bar.dart';
+import 'package:codeup/ui/post/createPost/drop_down_languages.dart';
 import 'package:codeup/ui/post/viewMosel/post_view_model.dart';
 import 'package:codeup/ui/saved_posts/saved_post_list.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../entities/Person.dart';
 import '../../entities/post.dart';
@@ -26,8 +28,10 @@ class CreatePostScreen extends StatefulWidget {
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
   final background_color = CustomColors.lightGrey3;
-  TextEditingController postController = TextEditingController();
-  String responseContent = "";
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contentController = TextEditingController();
+  String titleValue = "";
+  String contentValue = "";
   AuthService authService = AuthService();
   PostService postService = PostService();
 
@@ -40,6 +44,12 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
 
   @override
   Widget build(BuildContext context) {
+    const InputBorder border = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 1,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(6)),
+    );
     return Scaffold(
       backgroundColor: background_color,
       body: CustomScrollView(
@@ -49,25 +59,72 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
             delegate: SliverChildListDelegate([
               Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: Container(height: 200, color: Colors.white, child: Column(children: [
-                  Text("Title"),
-                  TextField(
-                            controller: postController,
-                            decoration: const InputDecoration(
-                              //labelStyle: TextStyle(color: Colors.orange),
-                              hintText: 'Add a reponse...',
+                child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                       /*  Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Image.network(
+              AuthService.currentUser!.photoUrl,
+              height: 120, 
+            
+      )), */
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            textInputAction: TextInputAction.next,
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.only(left: 15.0),
+                              hintText: "Title",
+                              hintStyle: GoogleFonts.notoSans(
+                                color: CustomColors.lightGrey1,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              border: border,
                             ),
-                            
+                            controller: titleController,
                             onChanged: (str) {
                               setState(() {
-                                responseContent = str;
+                                titleValue = str;
                               });
                             },
-                            onSubmitted: (str) {}),
-                      
-                    
-                  Text("Content")
-                ],),),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            
+                            textInputAction: TextInputAction.next,
+                            maxLines: 7,
+                            decoration: InputDecoration(
+                              
+                              isDense: true,
+                              contentPadding: const EdgeInsets.all(15.0),
+                              hintText: "What do you want to say ? ",
+                              hintStyle: GoogleFonts.notoSans(
+                                color: CustomColors.lightGrey1,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              border: border,
+                            ),
+                            controller: contentController,
+                            onChanged: (str) {
+                              setState(() {
+                                contentValue = str;
+                              });
+                            },
+                          ),
+                        ),
+                        DropDownLanguages()
+                      ],
+                    ),
+                  ),
+                ),
               ),
               /* Container(
                 decoration: BoxDecoration(color: background_color),
@@ -117,14 +174,14 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
   }
 
   _submitPost() async {
-      final response = await postService.addPost(
-          Post(-1, "Welcome", responseContent, "C", 1,
-              AuthService.currentUser!.user.id),
-          AuthService.currentUser!);
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
-          return HomeScreen();
-        }));
-      }
+    final response = await postService.addPost(
+        Post(-1, "Welcome", titleValue, "C", 1,
+            AuthService.currentUser!.user.id),
+        AuthService.currentUser!);
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return HomeScreen();
+      }));
+    }
   }
 }
